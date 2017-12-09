55efe3e7d2ec9cf83816dc459199c7bc731fdef6
Anton Chumakov <anton@chumakoff.com>
2017-12-09 03:21:53 +0300

    Check if attributes exist

--- lib/nullify_empty/active_record_extension.rb
+++ lib/nullify_empty/active_record_extension.rb
@@ -3,10 +3,10 @@ module NullifyEmpty
     def nullify_empty(*attr_names)
       return unless connection.table_exists? table_name
 
-      attr_names.each do |attr_name|
+      (column_names & attr_names.map(&:to_s)).each do |attr_name|
         original_type_klass = attribute_types[attr_name.to_s].class
 
-        new_type_klass = 
+        new_type_klass =
           NULLIFY_EMPTY_TYPES.find { |_, klass| klass.superclass == original_type_klass }.first
 
         attribute attr_name, new_type_klass
